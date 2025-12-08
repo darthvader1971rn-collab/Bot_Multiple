7f9892e0a032fad6f95db9715e2da78da2ae1cdf
DarthVader1971 <darthvader1971.rn@gmail.com>
2025-12-08 22:14:01 +0100

    poprawa sekwencji awaryjnej

---
+++ wymus_wysylanie.bat
@@ -0,0 +1,7 @@
+@echo off
+echo --- WYMUSZANIE WYSLANIA (FORCE PUSH) ---
+echo UWAGA: To nadpisze wszystko na GitHubie wersja z Twojego komputera.
+echo.
+git push -u origin main --force
+echo.
+pause
